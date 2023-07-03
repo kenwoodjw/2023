f4d42c2c7aa6b6295f060d2e3db59ec56fafdea5
kenwoodjw <blackxin55+@gmail.com>
2023-07-03 14:16:58 +0800

    Update __init__.py

--- github_daily/__init__.py
+++ github_daily/__init__.py
@@ -7,13 +7,13 @@ from .from_issues import get_info_from_issue_comments
 from .runner import GTDRunner
 from .shanbay import get_shanbay_daily
 
-# MY_STATUS_DICT_FROM_API = {
-#     "番茄": {
-#         "daily_func": get_forst_daily,
-#         "url": "https://github.com/yihong0618/2023/issues/12",
-#         "unit_str": " (个)",
-#     },
-# }
+MY_STATUS_DICT_FROM_API = {
+    # "番茄": {
+    #     "daily_func": get_forst_daily,
+    #     "url": "https://github.com/kenwoodjw/2023/issues/12",
+    #     "unit_str": " (个)",
+    # },
+}
 
 MY_STATUS_DICT_FROM_COMMENTS = {
     # "俯卧撑": {"daily_func": get_info_from_issue_comments, "unit_str": " (个)"},
